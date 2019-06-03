5cc00e0223567a431076dc38a25fc74f001223cb
Uriel Fluss <33055531+urielfluss@users.noreply.github.com>
2019-06-03 17:30:36 +0300

    upload har analyser for dev tool

---
+++ har_analyser/har_analyzer.py
@@ -0,0 +1,15 @@
+import json
+from haralyzer import HarParser
+
+with open('www.youtube.com.har', 'r', encoding='utf8') as f:
+    har_parser = HarParser(json.loads(f.read()))
+total_weight = 0
+count=0
+for page in har_parser.pages:
+    for entry in page.entries:
+        total_weight += entry['response']['content']['size']
+
+
+print("Total time: {0}(seconds)".format(har_parser.har_data['pages'][0]['pageTimings']['onLoad']/1000))
+print("Total requests: ", len(har_parser.har_data['entries']))
+print("Total weight: {0}(MB)".format(total_weight/1000000))
